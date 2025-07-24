07050c309416298a0441d17d5d2510d260622db7
Bison Hubert <bisonhubert@gmail.com>
2025-07-24 14:35:47 -0700

    Add file and sysread

--- tutorials_point/ruby_io.rb
+++ tutorials_point/ruby_io.rb
@@ -1,11 +1,30 @@
 #!/usr/bin/ruby
 
 # putc outputs one char at a time
-str = "Hello Ruby!"
-putc str
+# str = "Hello Ruby!"
+# putc str
 # -> "H"
 
 # puts and get
-puts "Enter a value:"
-val = gets
-puts val
+# puts "Enter a value:"
+# val = gets
+# puts val
+
+# File.new
+# aFile = File.new("filename", "mode")
+# ... process the file
+# aFile.close
+
+# File.open
+# File.open("filename", "mode") do |aFile|
+  # ... process the file
+# end
+
+# sysread
+aFile = File.new("input.txt", "r")
+if aFile
+  content = aFile.sysread(20)
+  puts content
+else
+  puts "Unable to open file!"
+end
