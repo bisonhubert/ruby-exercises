bc9f76bdfbfb8c01454b284c9c6bd965fbc9aced
Bison Hubert <bisonhubert@gmail.com>
2025-07-24 14:37:20 -0700

    Add syswrite

--- tutorials_point/ruby_io.rb
+++ tutorials_point/ruby_io.rb
@@ -21,10 +21,18 @@
 # end
 
 # sysread
-aFile = File.new("input.txt", "r")
+# aFile = File.new("input.txt", "r")
+# if aFile
+#   content = aFile.sysread(20)
+#   puts content
+# else
+#   puts "Unable to open file!"
+# end
+
+# syswrite
+aFile = File.new("input.txt", "r+")
 if aFile
-  content = aFile.sysread(20)
-  puts content
+  aFile.syswrite("ABCDEF")
 else
   puts "Unable to open file!"
 end
