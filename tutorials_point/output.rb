21ada460e6cdade7b6c81b0bebbc5ce673659e23
Bison Hubert <bisonhubert@gmail.com>
2025-07-24 14:42:03 -0700

    Add output

---
+++ tutorials_point/output.rb
@@ -0,0 +1,8 @@
+puts "This is output with 'puts' so it adds a newline after the output..."
+
+print "This is output with 'print', which does not have a newline after the output."
+
+p "This is output with 'p' and will \n print the newline \n and other special characters like \s as part of the output."
+
+puts "Compare above against this line, using 'puts': This is output with 'p' and will \n print the newline \n and other special characters like \s as part of the output."
+print "Compare above against this line, using 'print': This is output with 'p' and will \n print the newline \n and other special characters like \s as part of the output."
